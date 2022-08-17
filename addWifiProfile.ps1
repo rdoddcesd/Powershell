6cf6bb6754255b362a4cc201ca43cb7d48d96250
rdoddcesd <rdodd@clackesd.k12.or.us>
2022-08-17 13:57:00 -0700

    Update addWifiProfile.ps1
    Created parameters for variables.

--- addWifiProfile.ps1
+++ addWifiProfile.ps1
@@ -2,11 +2,14 @@
 #   netsh wlan show profiles
 # Choose the profile you want to export and a location to export the file to:
 #   netsh wlan export profile name="Ryan" folder="c:\wireless-profile" 
-# Place wifi profile XML file in SYSVOL
-# Path to wifi profile XML
-$prof = "C:\temp\Wi-Fi-Ryan.xml"
-# SSID
-$name = "Ryan"
 
-netsh wlan add profile filename=$prof user=all
-netsh wlan set profileparameter name=$name connectionmode=auto
+# example, .\addWifiProfile.ps1 -ssid Ryan -profile C:\temp\Wi-Fi-Ryan.xml
+
+param 
+    (
+        [Parameter(Mandatory=$true)] $ssid,
+        [Parameter(Mandatory=$true)] $profile
+    )
+
+netsh wlan add profile filename=$profile user=all
+netsh wlan set profileparameter name=$ssid connectionmode=auto
