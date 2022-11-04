9c15a2bdb196d5c75c028e247775fa9da7ba1bcc
rdoddcesd <rdodd@clackesd.k12.or.us>
2022-11-04 15:58:45 -0700

    Update deleteLogs.ps1

--- deleteLogs.ps1
+++ deleteLogs.ps1
@@ -1,11 +1,20 @@
-$logPath = 'E:\Winevt\Logs'
-$logSearch = 'E:\Winevt\Logs\Archive*'
-$logCount = 70
+# .\deleteLogs.ps1 -hostname ad02 -driveletter e -logcount 65
+
+param 
+    (
+        [Parameter(Mandatory=$true)] $hostname,
+        [Parameter(Mandatory=$true)] $driveletter,
+		[Parameter(Mandatory=$true)] $logCount
+    )
+
+$logPath = "\\" + $hostname + "\" + $driveletter + "$" + "\Winevt\Logs"
+$logSearch = "\\" + $hostname + "\" + $driveletter + "$" + "\Winevt\Logs\Archive*"
+$logCount = $logCount
 
 $itemList = Get-ChildItem -Path $logSearch | Sort-Object LastWriteTime -desc  | Select-Object -Skip $logCount
 
 ForEach ($item in $itemList) {
     $itemPath = $logPath + "\" + $item.name
     $itemPath
-    Remove-Item -LiteralPath $itemPath
+    #Remove-Item -LiteralPath $itemPath
 }
