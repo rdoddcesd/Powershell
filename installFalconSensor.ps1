19be730f26154731fdfbdf5d4a57273b639791e5
rdoddcesd <rdodd@clackesd.k12.or.us>
2023-05-18 08:37:21 -0700

    Update installFalconSensor.ps1

--- installFalconSensor.ps1
+++ installFalconSensor.ps1
@@ -1,22 +1,25 @@
-#-noninteractive -executionPolicy Bypass -noprofile -file \\tech.local\NETLOGON\MYSCRIPT.PS1
-
-# MUST ADD ORG SPICIFIC DATA HERE
-$installer = '<path to EXE>'
-$cid = '<customer ID checksum>'
+$installer = '\\ortrail.k12.or.us\SYSVOL\ortrail.k12.or.us\installers\FalconWindowsSensor6-40-15406.exe'
+$cid = '<falcon CID goes here>'
+$sophosPath = "C:\Program Files\Sophos"
 
 $installerArguments = "/install /quiet /norestart CID=$cid"
 $installedPath = 'C:\Program Files\CrowdStrike\CSFalconService.exe'
 
-"Test to see if installer is availible"
-if (Test-Path -Path $installer) {
-    "Path exists!"
-    "Test to see if Falcon in installed"
-    if (Test-Path -Path $installedPath) {
-        "Falcon is installed!"
+if($sophosPath) {
+    "Sophos is installed"
+} Else{
+    "Test if Falcon installer is availible"
+    if (Test-Path -Path $installer) {
+        "Path exists!"
+        "Test to see if Falcon in installed"
+        if (Test-Path -Path $installedPath) {
+            "Falcon is installed!"
+        } else {
+            Start-Process -FilePath $installer -ArgumentList $installerArguments -NoNewWindow
+            "Falcon has been installed!"
+        }
     } else {
-        Start-Process -FilePath $installer -ArgumentList $installerArguments -NoNewWindow
-        "Falcon has been installed!"
+        "Installer is not availible."
     }
-} else {
-    "Installer is not availible."
 }
+#powershell.exe -noninteractive -executionPolicy Bypass -noprofile -file \\tech.local\NETLOGON\MYSCRIPT.PS1
