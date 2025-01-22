c3c9b9db9883b3dbce9d9e9905c03a284e986783
rdoddcesd <rdodd@clackesd.k12.or.us>
2025-01-22 14:22:26 -0800

    Update cleanupUsers.ps1
    Added variables for AD and Feed file UniqueID
    
    Fixed if statement logic

--- cleanupUsers.ps1
+++ cleanupUsers.ps1
@@ -8,6 +8,10 @@
     CSV with header of userID containing list of active users.
 .PARAMETER searchOU
     OU of Users to be evaluated, search is recursive
+.PARAMETER adUniqueID
+    Active directory Unique user identifier, ie. sAMAccountName
+.PARAMETER feedUniqueID
+    Feed file Unique user identifier, ie. SamAccountName
 .PARAMETER logFile
     Location of Log file
 .NOTES
@@ -20,37 +24,35 @@
     Clackamas Education Service District - Technology
     rdodd@clackesd.k12.or.us
 .EXAMPLE
-    .\cleanupUsers.ps1 -feedFile .\data\users.csv -logFile .\log\disableUsersLog.txt -searchOU "OU=Students,DC=summitlc,DC=k12,DC=or,DC=us"
+    .\cleanupUsers.ps1 -feedFile .\data\users.csv -logFile .\log\disableUsersLog.txt -searchOU "OU=Students,DC=summitlc,DC=k12,DC=or,DC=us" -adUniqueID sAMAccountName -feedUniqueID SamAccountName
 #>
 
 Param
 (
     [Parameter(Mandatory = $true, HelpMessage = "CSV with header of userID")][string] $feedFile,
     [Parameter(Mandatory = $true, HelpMessage = "DN of OU to search")][string] $searchOU,
+    [Parameter(Mandatory = $true, HelpMessage = "Active directory Unique user identifier,")][string] $adUniqueID,
+    [Parameter(Mandatory = $true, HelpMessage = "Feed file Unique user identifier")][string] $feedUniqueID,
     [Parameter(Mandatory = $true, HelpMessage = "Log location")][string] $logFile
 )
 
-$users = Get-ADUser -Filter * -SearchBase $searchOU -Properties employeeID
+$users = Get-ADUser -Filter * -SearchBase $searchOU -Properties $adUniqueID
 $feed = Import-Csv -Path $feedFile
 $userObjectList = @()
 
 foreach ($user in $users) {
-    $adUserID = $user.employeeID
-    $feedUserID = $feed | Where-Object {$_.userID -match $adUserID} | select userID
+    $adUserID = $user.$adUniqueID
+    $feedUserID = $feed | Where-Object {$_.$feedUniqueID -match $adUserID} | Select-Object $feedUniqueID
     $adCurrentOU = ($user.DistinguishedName -split ",",2)[1]
-    
-    if(!$feedUserID[0]){
+
+    if(!$feedUserID){
         $adUserID
+        $currentUserObject = [PSCustomObject]@{
+            adUserID = $adUserID
+            adCurrentOU = $adCurrentOU
+        }
+        $userObjectList += $currentUserObject
     }
-
-    $currentUserObject = [PSCustomObject]@{
-        adSamAccountName = $user.samaccountname
-        adUserID = $adUserID
-        feedSamAccountName = $feedUserID
-        adCurrentOU = $adCurrentOU
-    }
-
-    $userObjectList += $currentUserObject
 }
 
 $userObjectList | Export-Csv -Path $logFile -NoTypeInformation
